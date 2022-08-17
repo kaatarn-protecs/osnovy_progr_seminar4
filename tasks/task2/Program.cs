c61383539cc97da10833cec62dc5553168ed7c60
kaatarn-protecs <golddez@gmail.com>
2022-08-17 19:09:33 +0300

    Задания с семинара

---
+++ tasks/task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Веедите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int NumCount(int number)
+{
+    int sum = 0;
+    for (int i = 1; number > 0; i++)
+    {
+        number = number / 10;
+        sum = i;
+    }
+    return sum;
+}
+
+int res = NumCount(num);
+Console.WriteLine($"Количество цифр в числе {num} = {res}");
---
+++ tasks/task4/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+Console.WriteLine("Введите размер массива ");
+int lenght = Convert.ToInt32(Console.ReadLine());
+
+int[] RandArray(int len)
+{
+    int[] arrays = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < arrays.Length; i++)
+    {
+        arrays[i] = rnd.Next(0, 2);
+    }
+    return arrays;
+}
+
+int[] res = RandArray(lenght);
+
+void PrintArray(int[] res1)
+{
+    Console.Write($"[ ");
+    for (int i = 0; i < res1.Length; i++)
+    {
+        Console.Write($"{res1[i]} ");
+    }
+    Console.Write($"]");
+}
+
+PrintArray(res);

c6df070ac8d555bbb84624f5528b95cc4f417420
kaatarn-protecs <golddez@gmail.com>
2022-08-18 09:15:44 +0300

    Решение второй задачи

---
+++ homework/task2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 27: Напишите программу, 
+// которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int aDigit = 0;
+while (true)
+{
+    Console.WriteLine("Введите новое число ");
+    aDigit = Convert.ToInt32(Console.ReadLine());
+    if (aDigit >= 0) break;
+    else 
+    {
+        Console.WriteLine("Вы ввели некорректное число!");
+        Console.WriteLine("Для выхода, нажмите CTRL + C");
+    }
+}
+
+int SumDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    if (sum == 0) return 1;
+    else return sum;
+}
+
+int res = SumDigits(aDigit);
+Console.WriteLine($"{aDigit} -> {res}");
